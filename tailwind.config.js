295c0ae109e2746050c7b9725a005d78541baaa0
sidiq20 <sidiqolasode@gmail.com>
2025-02-11 04:07:45 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'xfinity-blue': '#0272ce',
+        'xfinity-blue': '#7B1FA2', // Changed to purple
         'xfinity-dark': '#1b2b3a',
       }
     },
